671d295baf21dc2b50084d1f64de750bd14cdc72
Daniel Freedman <compismyrx@gmail.com>
2009-03-23 13:36:15 -0500

    Using Queues and Stacks, LR parser!

--- brainf.scala
+++ brainf.scala
@@ -1,13 +1,46 @@
 /** Brainfuck interpreter
  *  Written just to do it
  */
+import BrainFToken._
+import scala.collection.mutable.{Queue,Stack}
 class BrainF {
 	/*Parse a brainfuck string in and decode it to the output string.
 	 *Not sure how to handle bad input, but will figure it out later
 	 */
 	//All brainfuck programs are to operate within a 30,000 byte array.
-	var data = Array[Byte](30000)
+	private var data = new Array[Byte](30000);
 	//Instruction counter
-	var PC = 0;
-	//Token stack has a Token, and an Optional goto if Token == LB
-	var tokenStack:List[(BrainFToken,Option[Int])] = null
+	private var PC = 0;
+	//Token queue has a Token, and an Optional goto if Token == LB
+	private var tokenQueue = new Queue[(BrainFToken,Option[Int])];
+	private def goto(x:Int) = { PC = x }
+	/* Tokenize a string
+	 * Upon seeing a '[', push PC onto oldPC stack
+	 * Upon seeing a ']', pop oldPC stack into the optional Int portion of the tuple
+	 * This will allow for loops on parsing
+	 */
+	private def tokenize(x:String):Unit = {
+		tokenQueue.clear
+		var oldPC = new Stack[Int];
+		for (i <- new Range(0,x.length,1)) {
+			x(i) match {
+				case '<' => tokenQueue.enqueue((DP,None))
+				case '>' => tokenQueue.enqueue((IP,None))
+				case '+' => tokenQueue.enqueue((PP,None))
+				case '-' => tokenQueue.enqueue((MP,None))
+				case '.' => tokenQueue.enqueue((OUT,None))
+				case ',' => tokenQueue.enqueue((IN,None))
+				case '[' => {
+					oldPC.push(i)
+					tokenQueue.enqueue((LB,None))
+				}
+				case ']' => tokenQueue.enqueue((RB,Some(oldPC.pop())))
+				case _ => { /*?*/ }
+			}
+		}
+	}
+	def evalTokens(x:String):String = {
+		tokenize(x);
+		tokenQueue.mkString(",");
+	}
+}
