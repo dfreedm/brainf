bea908a831707ada614fe8942099386910d4acf7
Daniel Freedman <compismyrx@gmail.com>
2009-03-23 15:47:47 -0500

    Went to ArrayList to make backtracking MUCH easier

--- brainf.scala
+++ brainf.scala
@@ -2,17 +2,20 @@
  *  Written just to do it
  */
 import BrainFToken._
-import scala.collection.mutable.{Queue,Stack}
+import scala.collection.mutable.ArrayStack
+import scala.collection.jcl.ArrayList
 class BrainF {
 	/*Parse a brainfuck string in and decode it to the output string.
 	 *Not sure how to handle bad input, but will figure it out later
 	 */
 	//All brainfuck programs are to operate within a 30,000 byte array.
 	private var data = new Array[Byte](30000);
+	//Pointer to current data cell
+	private var dataPtr = 0
 	//Instruction counter
 	private var PC = 0;
-	//Token queue has a Token, and an Optional goto if Token == LB
-	private var tokenQueue = new Queue[(BrainFToken,Option[Int])];
+	//Token holder, array list for backtracking purposes
+	private var tokenList = new ArrayList[(BrainFToken,Option[Int])];
 	private def goto(x:Int) = { PC = x }
 	/* Tokenize a string
 	 * Upon seeing a '[', push PC onto oldPC stack
@@ -20,27 +23,33 @@ class BrainF {
 	 * This will allow for loops on parsing
 	 */
 	private def tokenize(x:String):Unit = {
-		tokenQueue.clear
-		var oldPC = new Stack[Int];
+		tokenList.clear
+		var oldPC = new ArrayStack[Int]
+		var level = 0
 		for (i <- new Range(0,x.length,1)) {
 			x(i) match {
-				case '<' => tokenQueue.enqueue((DP,None))
-				case '>' => tokenQueue.enqueue((IP,None))
-				case '+' => tokenQueue.enqueue((PP,None))
-				case '-' => tokenQueue.enqueue((MP,None))
-				case '.' => tokenQueue.enqueue((OUT,None))
-				case ',' => tokenQueue.enqueue((IN,None))
+				case '<' => tokenList.add((DP,None))
+				case '>' => tokenList.add((IP,None))
+				case '+' => tokenList.add((PP,None))
+				case '-' => tokenList.add((MP,None))
+				case '.' => tokenList.add((OUT,None))
+				case ',' => tokenList.add((IN,None))
 				case '[' => {
 					oldPC.push(i)
-					tokenQueue.enqueue((LB,None))
+					tokenList.add((LB,None))
 				}
-				case ']' => tokenQueue.enqueue((RB,Some(oldPC.pop())))
+				case ']' => {
+					tokenList.add((RB,Some(oldPC.peek)))
+					tokenList.update(oldPC.pop,(LB,Some(i)))
+				}
 				case _ => { /*?*/ }
 			}
 		}
 	}
+	/* Prints out token queue to check for accuracy */
 	def evalTokens(x:String):String = {
 		tokenize(x);
-		tokenQueue.mkString(",");
+		tokenList.mkString(",");
 	}
+	private def parse():String = {return null}
 }
